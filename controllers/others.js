afef24d0e0c2cf78e9d030239d77b6c3f0475e09
Kim Jungho <j21chon@gmail.com>
2021-06-11 09:46:38 +0900

    restore1

--- controllers/others.js
+++ controllers/others.js
@@ -11,48 +11,49 @@ module.exports = {
 
     })
   },
-  searchController: (req, res) => {
+  searchController: async (req, res) => {
+
     //  /search?searchType=nickname&searchWord=jordan
-    const { searchType, searchWord } = req.body;
-    if(searchType === 'nickname'){
-      post.findAll({
-        where: {
-          // Select * from post
-          // left join post_user on posts.id = post_user.postId
-          // left join users on post_user.userId = users.id
-          // where users.nickname = searchWord
-          searchWord: users.nickname    
-        },
-        include: [{
-          model: users,
-          required: false,
-        }, {
-          model: post_user,
-          required: false
-        }]
-      })
-    } else if(searchType === 'hashtag'){
-      post.findAll({
-          // Select * from post
-          // left join post_tag on posts.id = post_tag.postId
-          // left join tags on post_tag.tagId = tags.id
-          // where tags.name = searchWord        
-      })
-    } else {
-      post.findAll({
-        where: {
-          title: {
-            [Op.like]: '%' + searchWord + '%'
-          }
-        }
-      })
-      .then(result => {
-        res.status(200).send(result)
-      })
-      .catch(err => {
-        res.status(500).send('err')
-      })
-    }
+    // const { searchType, searchWord } = req.body;
+    // if(searchType === 'nickname'){
+    //   post.findAll({
+    //     where: {
+    //       // Select * from post
+    //       // left join post_user on posts.id = post_user.postId
+    //       // left join users on post_user.userId = users.id
+    //       // where users.nickname = searchWord
+    //       searchWord: users.nickname    
+    //     },
+    //     include: [{
+    //       model: users,
+    //       required: false,
+    //     }, {
+    //       model: post_user,
+    //       required: false
+    //     }]
+    //   })
+    // } else if(searchType === 'hashtag'){
+    //   post.findAll({
+    //       // Select * from post
+    //       // left join post_tag on posts.id = post_tag.postId
+    //       // left join tags on post_tag.tagId = tags.id
+    //       // where tags.name = searchWord        
+    //   })
+    // } else {
+    //   post.findAll({
+    //     where: {
+    //       title: {
+    //         [Op.like]: '%' + searchWord + '%'
+    //       }
+    //     }
+    //   })
+    //   .then(result => {
+    //     res.status(200).send(result)
+    //   })
+    //   .catch(err => {
+    //     res.status(500).send('err')
+    //   })
+    // }
 
     
 
